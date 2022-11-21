0dcadcd262e3df806c813296c1656cfa203848ed
Brennan <bredgley@gmail.com>
2022-11-21 16:10:08 -0700

    Made lambda function

---
+++ lambda.py
@@ -0,0 +1,10 @@
+import boto3  
+import json
+
+def lambda_handler(event, context):
+
+    sqs = boto3.resource('sqs')
+
+    queue = sqs.get_queue_by_name(QueueName='cs5260-requests')
+
+    response = queue.send_message(MessageBody=json.dumps(event))
